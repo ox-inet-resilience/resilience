6487c8eac10e998d49dfe558eced7e7d58ef7b2b
rht <rhtbot@protonmail.com>
2019-04-01 22:35:26 +0100

    Bump to version 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 dependency_links = ['git+https://github.com/ox-inet-resilience/py-distilledESL#egg=py-distilledESL']
 
 setup(name='resilience',
-      version='0.1',
+      version='0.2',
       description='System-wide stress testing',
       url='https://github.com/ox-inet-resilience/resilience',
       keywords='abm stress-testing',
