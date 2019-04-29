19326199553b610e0039c286ab8b130b7c17f96a
rht <rhtbot@protonmail.com>
2019-04-29 15:45:01 +0100

    Move the contract Revaluation to bailin repo

--- resilience/contracts/__init__.py
+++ resilience/contracts/__init__.py
@@ -7,7 +7,6 @@ from .Loan import Loan
 from .Other import Other
 from .Shares import Shares
 from .EquityHolding import EquityHolding
-from .Revaluation import Revaluation
 from .LongTermUnsecured import LongTermUnsecured
 #from .Bond import Bond
 #from .MaturityType import MaturityType
