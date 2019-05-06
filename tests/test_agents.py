7276d136a6b567a3bb4f7601f5c55709c4c3f10c
rht <rhtbot@protonmail.com>
2019-05-06 02:21:11 +0100

    Refactor test_agents

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -65,6 +65,14 @@ class TestBank:
         bank.LCR_den_initial = bank.lcr_constraint.get_HQLA() / 1.2
         assert bank.get_LCR() == pytest.approx(1.2)
 
+    def check_ratios(self, bank, rwa=None, lev=None, lcr=None):
+        if rwa:
+            assert bank.get_RWA_ratio() == pytest.approx(rwa)
+        if lev:
+            assert bank.get_leverage() == pytest.approx(lev)
+        if lcr:
+            assert bank.get_LCR() == pytest.approx(lcr)
+
     def apply_shock(self, bank):
         # apply shock to deposit
         bank.get_ledger().get_liabilities_of_type(Deposit)[0].principal = 10.5
@@ -78,7 +86,7 @@ class TestBank:
 
         # act
         bank.act()
-        assert bank.get_leverage() == pytest.approx(0.05)
+        self.check_ratios(bank, lev=0.05)
 
     def test_act_rwa_only(self, bank):
         set_constraints(1, 0, 0)
@@ -91,9 +99,8 @@ class TestBank:
 
         # act
         bank.act()
-        assert bank.get_leverage() == pytest.approx(0.045454545454545454547)
         # make sure that RWCR target is reached
-        assert bank.get_RWA_ratio() == pytest.approx(bank.RWCR_target)
+        self.check_ratios(bank, rwa=bank.RWCR_target, lev=0.045454545454545454547)
 
     def test_act_lcr_only(self, bank):
         set_constraints(0, 0, 1)
@@ -106,5 +113,4 @@ class TestBank:
 
         # act
         bank.act()
-        assert bank.get_leverage() == pytest.approx(0.045454545454545454547)
-        assert bank.get_LCR() == pytest.approx(1.2)
+        self.check_ratios(bank, rwa=0.09090909090909090909, lev=0.045454545454545454547, lcr=1.2)
